daf385656a853767d9a1f953f927355175deff1d
Sharmina Akhtar <sakht318@gmail.com>
2018-02-12 18:15:22 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNt(color) FROM bears WHERE color > 1"
+  "SELECT COUNT(color) FROM bears WHERE color > 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
@@ -27,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT name,age,gender,color,temperament,alive FROM bears WHERE temperament = 'agressive';"
+  "SELECT name, age, gender, color, temperament, alive FROM bears WHERE temperament = 'agressive';"
 end
